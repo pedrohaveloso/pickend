fa88936fba58cdeabc7c05f25fc1f3d5c9e8f903
Pedro Veloso <contatopedrohalves@gmail.com>
2024-04-17 20:55:54 -0300

    fix: seeds maps.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,19 +1,19 @@
 alias Pickend.Accounts
 
 Accounts.create_user(%{
-  full_name: "João de Freytas",
-  document: "40312304870",
-  email: "email@gmail.com",
-  password: "senha",
-  money: 1000,
-  type: :common
+  "full_name" => "João de Freytas",
+  "document" => "40312304870",
+  "email" => "email@gmail.com",
+  "password" => "senha",
+  "money" => 1000,
+  "type" => :common
 })
 
 Accounts.create_user(%{
-  full_name: "Venda 1234",
-  document: "12308912390",
-  email: "venda@gmail.com",
-  password: "senha",
-  money: 10000,
-  type: :merchant
+  "full_name" => "Venda 1234",
+  "document" => "12308912390",
+  "email" => "venda@gmail.com",
+  "password" => "senha",
+  "money" => 10000,
+  "type" => :merchant
 })
