e9fafdb0de93c1c194ac9f442af46094c0131b9d
Pedro Veloso <contatopedrohalves@gmail.com>
2024-04-17 13:35:31 -0300

    feat: user create error read body msg.

--- lib/pickend_web/controllers/user_controller.ex
+++ lib/pickend_web/controllers/user_controller.ex
@@ -3,38 +3,38 @@ defmodule PickendWeb.UserController do
 
   alias Pickend.Accounts
 
-  def create(conn) do
-    case read_json_body(conn) do
-      {:ok, body} ->
-        email = if is_nil(body["email"]), do: "", else: body["email"]
-        document = if is_nil(body["document"]), do: "", else: body["document"]
+  def create(conn) when {:ok, body} = read_json_body(conn) do
+    email = if is_nil(body["email"]), do: "", else: body["email"]
+    document = if is_nil(body["document"]), do: "", else: body["document"]
 
-        {status, body} =
-          with nil <- Accounts.get_user(email: email),
-               nil <- Accounts.get_user(document: document),
-               {:ok, struct} <- Accounts.create_user(body) do
-            {200, struct}
-          else
-            %Accounts.User{} ->
-              {404, %{reason: "User e-mail/document already exists."}}
+    {status, body} =
+      with nil <- Accounts.get_user(email: email),
+           nil <- Accounts.get_user(document: document),
+           {:ok, struct} <- Accounts.create_user(body) do
+        {200, struct}
+      else
+        %Accounts.User{} ->
+          {404, %{reason: "User e-mail/document already exists."}}
 
-            {:error, changeset} ->
-              {404,
-               %{
-                 reason: "Invalid data.",
-                 errors:
-                   Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
-                     Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
-                       opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
-                     end)
-                   end)
-               }}
-          end
+        {:error, changeset} ->
+          {404,
+           %{
+             reason: "Invalid data.",
+             errors:
+               Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+                 Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
+                   opts
+                   |> Keyword.get(String.to_existing_atom(key), key)
+                   |> to_string()
+                 end)
+               end)
+           }}
+      end
 
-        send_json_resp(conn, status, body)
+    send_json_resp(conn, status, body)
+  end
 
-      _ ->
-        send_json_resp(conn, 400, %{reason: "Incorrect body."})
-    end
+  def create(conn) when {:error, reason} = read_json_body(conn) do
+    send_json_resp(conn, 400, %{reason: reason})
   end
 end
