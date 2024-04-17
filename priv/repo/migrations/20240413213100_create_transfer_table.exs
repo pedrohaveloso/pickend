7054cd5419fba4e0ab0c7bd59ac8ba576ac8860d
Pedro Veloso <contatopedrohalves@gmail.com>
2024-04-17 17:19:58 -0300

    fix: transfers table name.

--- priv/repo/migrations/20240413213100_create_transfer_table.exs
+++ priv/repo/migrations/20240413213100_create_transfer_table.exs
@@ -1,8 +1,8 @@
-defmodule Pickend.  Repo.Migrations.CreateTransferTable do
+defmodule Pickend.Repo.Migrations.CreateTransferTable do
   use Ecto.Migration
 
   def change() do
-    create table("transfer", primary_key: false) do
+    create table("transfers", primary_key: false) do
       add(:id, :binary_id, primary_key: true)
 
       add(:amount, :integer)
