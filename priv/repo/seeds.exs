cf5583c85b93b5da27f3acb2325b3d1ffeae9bcd
Pedro Veloso <contatopedrohalves@gmail.com>
2024-04-16 22:26:50 -0300

    feat: seeds example users.

---
+++ priv/repo/seeds.exs
@@ -0,0 +1,19 @@
+alias Pickend.Accounts
+
+Accounts.create_user(%{
+  full_name: "João de Freytas",
+  document: "40312304870",
+  email: "email@gmail.com",
+  password: "senha",
+  money: 1000,
+  type: :common
+})
+
+Accounts.create_user(%{
+  full_name: "Venda 1234",
+  document: "12308912390",
+  email: "venda@gmail.com",
+  password: "senha",
+  money: 10000,
+  type: :merchant
+})
