29e71373d2aac091be2260ea59a834f0ee5ba1c6
Pedro Veloso <contatopedrohalves@gmail.com>
2024-04-19 16:22:42 -0300

    feat: create transfer route.

--- lib/pickend_web/controllers/transfer_controller.ex
+++ lib/pickend_web/controllers/transfer_controller.ex
@@ -1,6 +1,36 @@
 defmodule PickendWeb.TransferController do
   use PickendWeb, :controller
 
-  def create(_conn) do
+  alias Pickend.{Financial, Accounts}
+
+  def create(conn) do
+    case read_json_body(conn) do
+      {:ok, body, _conn} ->
+        create(conn, body)
+
+      {:error, reason} ->
+        send_json_resp(conn, 400, %{"reason" => reason})
+    end
+  end
+
+  def create(conn, body) do
+    with {:ok, struct} <- Financial.create_transfer(body),
+         {:ok, _} <-
+           Accounts.update_user_money(
+             body["payee"],
+             String.to_integer(body["amount"]),
+             :add
+           ),
+         {:ok, _} <-
+           Accounts.update_user_money(
+             body["payer"],
+             String.to_integer(body["amount"]),
+             :remove
+           ) do
+      send_json_resp(conn, 200, struct)
+    else
+      {:error, _} ->
+        send_json_resp(conn, 400, %{"reason" => "invalid data"})
+    end
   end
 end
